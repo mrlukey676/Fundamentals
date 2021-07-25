7f88e083b5db5dc7b51eaf1c75d86397ac905a8a
mrlukey676 <olitaylor52@gmail.com>
2021-07-25 10:38:58 +0100

    Update

--- src/main/java/me/admintools/admintools/Admintools.java
+++ src/main/java/me/admintools/admintools/Admintools.java
@@ -1,17 +1,24 @@
 package me.admintools.admintools;
 
 import me.admintools.admintools.commands.*;
+import me.admintools.admintools.files.warps;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Admintools extends JavaPlugin {
 
+    public static Admintools plugin;
     String version = getConfig().getString("plugin-version");
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        plugin = this;
         getConfig().options().copyDefaults();
         saveDefaultConfig();
+        warps.setup();
+        warps.get().addDefault("DO NOT WRITE IN HERE", ("DO NOT WRITE IN HERE"));
+        warps.get().options().copyDefaults(true);
+        warps.save();
         System.out.println("[AdminTools] Loaded version " + version);
         getCommand("fly").setExecutor(new fly());
         getCommand("vanish").setExecutor(new vanish());
@@ -25,6 +32,8 @@ public final class Admintools extends JavaPlugin {
         getCommand("setspawn").setExecutor(new setspawn());
         getCommand("day").setExecutor(new day());
         getCommand("night").setExecutor(new night());
+        getCommand("spawn").setExecutor(new spawn());
+        getCommand("newwarp").setExecutor(new newwarp());
     }
 
     @Override
---
+++ src/main/java/me/admintools/admintools/files/warps.java
@@ -0,0 +1,39 @@
+package me.admintools.admintools.files;
+
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+
+public class warps {
+    private static File file;
+    private static FileConfiguration customFile;
+
+    public static void setup(){
+        file = new File(Bukkit.getServer().getPluginManager().getPlugin("Admintools").getDataFolder(), "warps.yml");
+
+        if(!file.exists()){
+            try{
+                file.createNewFile();
+            }catch (IOException e){
+                //eeeeee
+            }
+        }
+        customFile = YamlConfiguration.loadConfiguration(file);
+    }
+    public static FileConfiguration get(){
+        return customFile;
+    }
+    public static void save(){
+        try{
+            customFile.save(file);
+        }catch (IOException e){
+            System.out.println("Couldn't save file!");
+        }
+    }
+    public static void reload(){
+        customFile = YamlConfiguration.loadConfiguration(file);
+    }
+}
