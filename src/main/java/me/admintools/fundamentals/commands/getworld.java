0703eddba27e8d3dc4050494b48c0b7295f5c970
mrlukey676 <87177010+676org@users.noreply.github.com>
2021-11-05 07:17:34 +0000

    New command: /getworld

---
+++ src/main/java/me/admintools/fundamentals/commands/getworld.java
@@ -0,0 +1,21 @@
+package me.admintools.fundamentals.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class getworld implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String s, String[] strings) {
+        if (sender instanceof Player){
+            Player player = (Player) sender;
+            if (player.hasPermission("fundamentals.getworld")){
+                player.sendMessage("§aYour current world is §e" + player.getWorld().getName());
+            }else{
+                player.sendMessage("§cYou do not have permission to use this command!");
+            }
+        }
+        return false;
+    }
+}
